195d2c15a7d25b0b4ff92d9a99abc12e5f4dd3a0
denis.fortin <denis.fortin@adevinta.com>
2022-05-12 16:20:43 +0200

    feat(local): add build script and example config file
    Change-Id: Iea3e29b6e12046f360641277212df943ec4f84e3

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# this script packages up all the binaries, and a script (deploy.sh)
+# to twiddle with the server and the binaries
+
+set -ex
+
+set -u
+
+out=zoekt-bin
+mkdir -p ${out}
+
+for d in $(find cmd  -maxdepth 1 -type d)
+do
+  go build -tags netgo -ldflags "-X github.com/google/zoekt.Version=dev"  -o ${out}/$(basename $d) github.com/google/zoekt/$d
+done
+
+chmod 755 ${out}/*
